bee3e1be01f7157a44cf7859e595464ac106975b
alexm15 <alexander.markussen@live.dk>
2020-06-20 16:45:48 +0200

    Removed unused projects

--- Webshop.UI/ViewModels/AssignedCategoryData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Webshop.UI.ViewModels
-{
-    public class AssignedCategoryData
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public bool Assigned { get; set; }
-    }
-}
